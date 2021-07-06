123ec7831cdfd4b00c1192e89230e39aeded553c
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-07-06 18:56:27 +0200

    Initial

---
+++ API/API/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mistaken.API
+{
+    public abstract class AutoUpdatablePlugin<TConfig> : Plugin<TConfig> where TConfig : IAutoUpdatableConfig, new()
+    {
+
+    }
+
+    public interface IAutoUpdatableConfig : IConfig
+    {
+
+    }
+}
