287445e12a24273defc12889a4574923e5b7ee89
Wydział Wewnętrzny <gamer@mistaken.pl>
2021-10-17 13:09:54 +0200

    fix 914 teleporting

---
+++ API/Patches/InRangePatch.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="InRangePatch.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandSystem;
+using CommandSystem.Commands.RemoteAdmin.Doors;
+using HarmonyLib;
+using Interactables.Interobjects.DoorUtils;
+using Scp914;
+using UnityEngine;
+
+namespace Mistaken.API.Patches
+{
+    [HarmonyPatch(typeof(Scp914Upgrader), nameof(Scp914Upgrader.Upgrade))]
+    internal static class InRangePatch
+    {
+        private static void Prefix(ref Collider[] intake, Vector3 moveVector, Scp914Mode mode, Scp914KnobSetting setting)
+        {
+            intake = intake.Where(x => x.GetComponent<Components.InRange>() == null && x.GetComponent<Components.InRangeBall>() == null).ToArray();
+        }
+    }
+}
