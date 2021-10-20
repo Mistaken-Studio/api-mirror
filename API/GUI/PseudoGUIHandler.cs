466491e1888c090ca29180e2fe076f2a8b39762a
Wydział Wewnętrzny <gamer@mistaken.pl>
2021-10-20 09:50:40 +0200

    threading?

--- API/GUI/PseudoGUIHandler.cs
+++ API/GUI/PseudoGUIHandler.cs
@@ -6,6 +6,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Exiled.API.Features;
 using MEC;
 using UnityEngine;
@@ -34,7 +36,11 @@ namespace Mistaken.API.GUI
         /// Stops updating GUI.
         /// </summary>
         /// <param name="p">player to ignore.</param>
-        public static void Ignore(Player p) => ToIgnore.Add(p);
+        public static void Ignore(Player p)
+        {
+            ToIgnore.Add(p);
+            ToUpdate.Remove(p);
+        }
 
         /// <summary>
         /// Starts updating GUI.
@@ -54,8 +60,7 @@ namespace Mistaken.API.GUI
 
         internal static void Set(Player player, string key, PseudoGUIPosition type, string content)
         {
-            bool remove = string.IsNullOrWhiteSpace(content);
-            if (remove)
+            if (string.IsNullOrWhiteSpace(content))
             {
                 if (!CustomInfo.TryGetValue(player, out Dictionary<string, (string, PseudoGUIPosition)> value) || !value.ContainsKey(key))
                     return;
@@ -76,17 +81,71 @@ namespace Mistaken.API.GUI
         private static readonly Dictionary<Player, Dictionary<string, (string Content, PseudoGUIPosition Type)>> CustomInfo = new Dictionary<Player, Dictionary<string, (string Content, PseudoGUIPosition Type)>>();
         private static readonly List<Player> ToUpdate = new List<Player>();
         private static readonly HashSet<Player> ToIgnore = new HashSet<Player>();
+        private readonly Dictionary<Player, string> constructedStrings = new Dictionary<Player, string>();
         private int frames = 0;
+        private Task guiCalculationThread;
+        private bool active = true;
 
         private void Start()
         {
             Instance = this;
             Exiled.Events.Handlers.Server.RestartingRound += this.Server_RestartingRound;
+
+            this.active = true;
+            this.guiCalculationThread = Task.Run(async () =>
+            {
+                while (this.active)
+                {
+                    await Task.Delay(100);
+
+                    this.frames += 1;
+
+                    // 10s
+                    if (this.frames > 99)
+                    {
+                        foreach (var item in RealPlayers.List)
+                        {
+                            try
+                            {
+                                if (!ToIgnore.Contains(item))
+                                    this.ConstructString(item);
+                            }
+                            catch (Exception ex)
+                            {
+                                Log.Error(ex.Message);
+                                Log.Error(ex.StackTrace);
+                            }
+                        }
+
+                        ToUpdate.Clear();
+                        this.frames = 0;
+                        continue;
+                    }
+
+                    if (ToUpdate.Count == 0)
+                        continue;
+                    foreach (var item in ToUpdate.ToArray())
+                    {
+                        try
+                        {
+                            if (item.IsConnected && !ToIgnore.Contains(item))
+                                this.ConstructString(item);
+                            ToUpdate.Remove(item);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex.Message);
+                            Log.Error(ex.StackTrace);
+                        }
+                    }
+                }
+            });
         }
 
         private void Destroy()
         {
             Exiled.Events.Handlers.Server.RestartingRound -= this.Server_RestartingRound;
+            this.active = false;
         }
 
         private void Server_RestartingRound()
@@ -94,41 +153,21 @@ namespace Mistaken.API.GUI
             CustomInfo.Clear();
             ToUpdate.Clear();
             ToIgnore.Clear();
+            this.constructedStrings.Clear();
         }
 
         private void FixedUpdate()
         {
-            this.frames += 1;
-            if (this.frames > 500)
-            {
-                foreach (var item in RealPlayers.List)
-                {
-                    try
-                    {
-                        if (item.IsConnected && !ToIgnore.Contains(item))
-                            this.UpdateGUI(item);
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.Error(ex.Message);
-                        Log.Error(ex.StackTrace);
-                    }
-                }
-
-                ToUpdate.Clear();
-                this.frames = 0;
+            if (this.constructedStrings.Count == 0)
                 return;
-            }
-
-            if (ToUpdate.Count == 0)
-                return;
-            foreach (var item in ToUpdate.ToArray())
+            foreach (var item in this.constructedStrings.Keys.ToArray())
             {
                 try
                 {
-                    if (item.IsConnected && !ToIgnore.Contains(item))
+                    if (!item.IsConnected)
+                        this.constructedStrings.Remove(item);
+                    else if (!ToIgnore.Contains(item))
                         this.UpdateGUI(item);
-                    ToUpdate.Remove(item);
                 }
                 catch (Exception ex)
                 {
@@ -138,7 +177,7 @@ namespace Mistaken.API.GUI
             }
         }
 
-        private void UpdateGUI(Player player)
+        private void ConstructString(Player player)
         {
             if (!CustomInfo.ContainsKey(player))
                 CustomInfo[player] = new Dictionary<string, (string Content, PseudoGUIPosition Type)>();
@@ -201,9 +240,15 @@ namespace Mistaken.API.GUI
                 toWrite += "<br>";
             toWrite += bottomContent;
 
-            player.ShowHint($"<size=75%><color=#FFFFFFFF>{toWrite}</color><br><br><br><br><br><br><br><br><br><br></size>", 7200);
+            this.constructedStrings[player] = $"<size=75%><color=#FFFFFFFF>{toWrite}</color><br><br><br><br><br><br><br><br><br><br></size>";
+        }
 
-            // Log.Debug($"Updating {player.Id} with {toWrite}");
+        private void UpdateGUI(Player player)
+        {
+            if (!this.constructedStrings.TryGetValue(player, out string text))
+                return;
+            player.ShowHint(text, 7200);
+            this.constructedStrings.Remove(player);
         }
     }
 }
