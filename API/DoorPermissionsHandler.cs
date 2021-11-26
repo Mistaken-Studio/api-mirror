e9a5942acb49ded836893f41fe50f1b82a0d29c9
Wydział Wewnętrzny <gamer@mistaken.pl>
2021-11-26 11:49:23 +0000

    Fix DoorPermissionsHandler.cs

--- API/DoorPermissionsHandler.cs
+++ API/DoorPermissionsHandler.cs
@@ -1,10 +1,11 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="DoorPermissionsHandler.cs" company="Mistaken">
 // Copyright (c) Mistaken. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
 
 using Exiled.API.Enums;
+using Exiled.API.Features.Items;
 using Exiled.API.Interfaces;
 using Mistaken.API.Diagnostics;
 using Mistaken.API.Extensions;
@@ -23,19 +24,77 @@ namespace Mistaken.API
         public override void OnEnable()
         {
             Exiled.Events.Handlers.Player.InteractingDoor += this.Player_InteractingDoor;
+            Exiled.Events.Handlers.Player.InteractingLocker += Player_InteractingLocker;
+            Exiled.Events.Handlers.Player.UnlockingGenerator += Player_UnlockingGenerator;
         }
 
         public override void OnDisable()
         {
             Exiled.Events.Handlers.Player.InteractingDoor -= this.Player_InteractingDoor;
+            Exiled.Events.Handlers.Player.InteractingLocker -= Player_InteractingLocker;
+            Exiled.Events.Handlers.Player.UnlockingGenerator -= Player_UnlockingGenerator;
+        }
+
+        private void Player_UnlockingGenerator(Exiled.Events.EventArgs.UnlockingGeneratorEventArgs ev)
+        {
+            if (ev.IsAllowed)
+                return;
+
+            if (ev.Player.IsCuffed)
+                return;
+
+            if (!ev.Player.TryGetSessionVariable<KeycardPermissions>(SessionVarType.BUILTIN_DOOR_ACCESS, out var value))
+                return;
+
+            KeycardPermissions basePermissions = (ev.Player.CurrentItem as Keycard)?.Permissions ?? KeycardPermissions.None;
+
+            if ((basePermissions & (KeycardPermissions)ev.Generator._requiredPermission) != 0)
+                return; // Keycard gives access, but still ev.IsAllowed = false, so maybe plugin denied it?
+
+            if (((value | basePermissions) & (KeycardPermissions)ev.Generator._requiredPermission) != 0)
+                ev.IsAllowed = true;
+        }
+
+        private void Player_InteractingLocker(Exiled.Events.EventArgs.InteractingLockerEventArgs ev)
+        {
+            if (ev.IsAllowed)
+                return;
+
+            if (ev.Player.IsCuffed)
+                return;
+
+            if (!ev.Player.TryGetSessionVariable<KeycardPermissions>(SessionVarType.BUILTIN_DOOR_ACCESS, out var value))
+                return;
+
+            KeycardPermissions basePermissions = (ev.Player.CurrentItem as Keycard)?.Permissions ?? KeycardPermissions.None;
+
+            if ((basePermissions & (KeycardPermissions)ev.Chamber.RequiredPermissions) != 0)
+                return; // Keycard gives access, but still ev.IsAllowed = false, so maybe plugin denied it?
+
+            if (((value | basePermissions) & (KeycardPermissions)ev.Chamber.RequiredPermissions) != 0)
+                ev.IsAllowed = true;
         }
 
         private void Player_InteractingDoor(Exiled.Events.EventArgs.InteractingDoorEventArgs ev)
         {
-            if (ev.IsAllowed) return;
-            if (ev.Door.IsLocked) return;
-            var value = ev.Player.GetSessionVariable<KeycardPermissions>(SessionVarType.BUILTIN_DOOR_ACCESS);
-            if ((value & (KeycardPermissions)ev.Door.RequiredPermissions.RequiredPermissions) != 0)
+            if (ev.IsAllowed) 
+                return;
+
+            if (ev.Player.IsCuffed)
+                return;
+
+            if (ev.Door.IsLocked)
+                return;
+
+            if (!ev.Player.TryGetSessionVariable<KeycardPermissions>(SessionVarType.BUILTIN_DOOR_ACCESS, out var value))
+                return;
+
+            KeycardPermissions basePermissions = (ev.Player.CurrentItem as Keycard)?.Permissions ?? KeycardPermissions.None;
+
+            if ((basePermissions & (KeycardPermissions)ev.Door.RequiredPermissions.RequiredPermissions) != 0)
+                return; // Keycard gives access, but still ev.IsAllowed = false, so maybe plugin denied it?
+
+            if (((value | basePermissions) & (KeycardPermissions)ev.Door.RequiredPermissions.RequiredPermissions) != 0)
                 ev.IsAllowed = true;
         }
     }
