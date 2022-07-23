6e30c148c530f6d7e4667f3bfc8245c20a9738d4
Xname <xnameserver@o2.pl>
2022-07-23 20:37:40 +0200

    transpiler yea

---
+++ API/Patches/PlayerHasHintPatch.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PlayerHasHintPatch.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Hints;
+
+#pragma warning disable SA1118 // Parameters should span multiple lines
+
+namespace Mistaken.API.Patches
+{
+    [HarmonyPatch(typeof(HintDisplay), nameof(HintDisplay.Show))]
+    internal static class PlayerHasHintPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> newInstructions = NorthwoodLib.Pools.ListPool<CodeInstruction>.Shared.Rent(instructions);
+
+            newInstructions.InsertRange(1, new CodeInstruction[]
+            {
+                new CodeInstruction(OpCodes.Ldnull),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(UnityEngine.Object), "op_Inequality")),
+            });
+
+            for (int i = 0; i < newInstructions.Count; i++)
+                yield return newInstructions[i];
+
+            NorthwoodLib.Pools.ListPool<CodeInstruction>.Shared.Return(newInstructions);
+
+            yield break;
+        }
+    }
+}
