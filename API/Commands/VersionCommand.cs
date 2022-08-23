0d8845b8e799683479f5e675440d40631c5afc2e
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-08-23 21:06:42 +0200

    .version command

---
+++ API/Commands/VersionCommand.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="VersionCommand.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Reflection;
+using CommandSystem;
+using Mistaken.API.Commands;
+
+namespace Mistaken.CommandsExtender.Commands
+{
+    [CommandSystem.CommandHandler(typeof(CommandSystem.ClientCommandHandler))]
+    internal class VersionCommand : IBetterCommand
+    {
+        public override string Command => "version";
+
+        public override string[] Execute(ICommandSender sender, string[] args, out bool success)
+        {
+            List<string> tor = new List<string>()
+            {
+                "Mistaken Studio's plugin versions list:",
+            };
+
+            foreach (var item in Exiled.Loader.Loader.PluginAssemblies)
+            {
+                var att = item.Key.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+                if (att is null)
+                    continue;
+
+                tor.Add($"[{item.Value.Author}.{item.Value.Name}] {att.InformationalVersion}{(att.InformationalVersion.EndsWith("0") ? " <color=red>DEVELOPMENT BUILD</color>" : string.Empty)}");
+            }
+
+            success = true;
+            return tor.ToArray();
+        }
+    }
+}
