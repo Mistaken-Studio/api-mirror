7f660cd100e5949e38cd454bd2b1e3178252335e
Xname <xnameserver@o2.pl>
2022-08-29 17:46:35 +0200

    fix

---
+++ API/Patches/NetworkConnectionIdentityPatch.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NetworkConnectionIdentityPatch.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using HarmonyLib;
+using InventorySystem.Disarming;
+using InventorySystem.Items.Firearms.BasicMessages;
+using Mirror;
+
+namespace Mistaken.API.Patches
+{
+    [HarmonyPatch(typeof(FirearmBasicMessagesHandler), nameof(FirearmBasicMessagesHandler.ServerShotReceived))]
+    [HarmonyPatch(typeof(DisarmingHandlers), nameof(DisarmingHandlers.ServerProcessDisarmMessage))]
+    internal static class NetworkConnectionIdentityPatch
+    {
+        private static bool Prefix(NetworkConnection conn)
+        {
+            return !conn.identity;
+        }
+    }
+}
