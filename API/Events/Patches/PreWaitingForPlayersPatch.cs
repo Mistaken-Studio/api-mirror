3f9a2ae6de7e4170db8cc57b003a59a96f23fcab
Bartosz Barwiński <barwinski.b@protonmail.com>
2022-08-30 20:34:14 +0200

    Added new events

---
+++ API/Events/Patches/PreWaitingForPlayersPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace Mistaken.API.Events.Patches
+{
+    [HarmonyPatch(typeof(Exiled.Events.Handlers.Server), nameof(Exiled.Events.Handlers.Server.OnWaitingForPlayers))]
+    internal static class PreWaitingForPlayersPatch
+    {
+        internal static bool Prefix()
+        {
+            Events.OnPreWaitingForPlayersEvent();
+            return true;
+        }
+    }
+}
