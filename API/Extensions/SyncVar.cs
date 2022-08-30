d2f66043a02f9e912bcd6dd810d7754310a96dcc
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-08-30 19:47:37 +0200

    add more Backwards Compatibility

---
+++ API/Extensions/SyncVar.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SyncVar.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Exiled.API.Features;
+
+namespace Mistaken.API.Extensions
+{
+    internal static class SyncVar
+    {
+        private static void TargetSetBadge(this Player player, Player target, string name, string color)
+            => SyncVarExtensions.TargetSetBadge(player, target, name, color);
+
+        private static void TargetSetNickname(this Player player, Player target, string nickname)
+            => SyncVarExtensions.TargetSetNickname(player, target, nickname);
+
+        private static void ChangeAppearance(this Player player, Player target, RoleType type)
+            => SyncVarExtensions.ChangeAppearance(player, target, type);
+    }
+}
--- API/Extensions/SyncVarExtensions.cs
+++ API/Extensions/SyncVarExtensions.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System;
 using Exiled.API.Extensions;
 using Exiled.API.Features;
 
