324d753db913f61a63c54f35472cf8447ced1151
Xname <xnameserver@o2.pl>
2022-09-05 17:24:42 +0200

    Add new spawnable structures

--- API/MapPlus.cs
+++ API/MapPlus.cs
@@ -11,9 +11,12 @@ using System.Linq;
 using AdminToys;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
+using Interactables.Interobjects.DoorUtils;
 using InventorySystem;
 using InventorySystem.Items;
 using InventorySystem.Items.Pickups;
+using MapGeneration;
+using MapGeneration.Distributors;
 using Mirror;
 using NorthwoodLib.Pools;
 using RemoteAdmin;
@@ -303,6 +306,40 @@ namespace Mistaken.API
             return ptoy;
         }
 
+        /// <summary>
+        /// Spawn's structures.
+        /// </summary>
+        /// <param name="type">Structure type.</param>
+        /// <param name="position">Structure's position.</param>
+        /// <param name="rotation">Structure's rotation.</param>
+        /// <returns>Spawned structure.</returns>
+        public static GameObject SpawnStructure(StructureType type, Vector3 position, Quaternion rotation)
+        {
+            if (!NetworkClient.prefabs.TryGetValue(StructurePrefabs[type], out GameObject prefab))
+                return null;
+
+            GameObject obj = UnityEngine.Object.Instantiate(prefab, position, rotation);
+            NetworkServer.Spawn(obj);
+            return obj;
+        }
+
+        /// <summary>
+        /// Spawn's structures.
+        /// </summary>
+        /// <param name="type">Structure type.</param>
+        /// <param name="parent">Structure's parent.</param>
+        /// <returns>Spawned structure.</returns>
+        public static GameObject SpawnStructure(StructureType type, Transform parent)
+        {
+            if (!NetworkClient.prefabs.TryGetValue(StructurePrefabs[type], out GameObject prefab))
+                return null;
+
+            GameObject obj = UnityEngine.Object.Instantiate(prefab, parent.position, parent.rotation);
+            obj.transform.SetParent(parent);
+            NetworkServer.Spawn(obj);
+            return obj;
+        }
+
         internal static readonly HashSet<AdminToyBase> SyncToyPosition = new HashSet<AdminToyBase>();
 
         internal static void PostRoundCleanup()
@@ -311,6 +348,24 @@ namespace Mistaken.API
             recontainer = null;
             SyncToyPosition.Clear();
         }
+
+        private static readonly Dictionary<StructureType, Guid> StructurePrefabs = new Dictionary<StructureType, Guid>()
+        {
+            { StructureType.Scp018Pedestal, new Guid("a149d3eb-11bd-de24-f9dd-57187f5771ef") },
+            { StructureType.Scp207Pedestal, new Guid("17054030-9461-d104-5b92-9456c9eb0ab7") },
+            { StructureType.Scp244Pedestal, new Guid("fa602fdc-724c-d2a4-8b8c-1fb314b82746") },
+            { StructureType.Scp268Pedestal, new Guid("68f13209-e652-6024-2b89-0f75fb88a998") },
+            { StructureType.Scp500Pedestal, new Guid("f4149b66-c503-87a4-0b93-aabfe7c352da") },
+            { StructureType.Scp1853Pedestal, new Guid("4f36c701-ea0c-9064-2a58-2c89240e51ba") },
+            { StructureType.Scp2176Pedestal, new Guid("fff1c10c-a719-bea4-d95c-3e262ed03ab2") },
+            { StructureType.RegularMedkitLocker, new Guid("5b227bd2-1ed2-8fc4-2aa1-4856d7cb7472") },
+            { StructureType.AdrenalineMedkitLocker, new Guid("db602577-8d4f-97b4-890b-8c893bfcd553") },
+            { StructureType.RifleRackLocker, new Guid("850f84ad-e273-1824-8885-11ae5e01e2f4") },
+            { StructureType.MiscLocker, new Guid("d54bead1-286f-3004-facd-74482a872ad8") },
+            { StructureType.LargeGunLocker, new Guid("5ad5dc6d-7bc5-3154-8b1a-3598b96e0d5b") },
+            { StructureType.Generator, new Guid("daf3ccde-4392-c0e4-882d-b7002185c6b8") },
+            { StructureType.Workstation, new Guid("ad8a455f-062d-dea4-5b47-ac9217d4c58b") },
+        };
 
         private static Recontainer079 recontainer;
         private static LureSubjectContainer container;
