9a95f82c9b27da5f8c0528f870bf9e32b77f0eaa
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-09-27 00:14:00 +0200

    It works

--- API/Toys/ToyHandler.cs
+++ API/Toys/ToyHandler.cs
@@ -6,10 +6,17 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AdminToys;
+using Exiled.API.Enums;
+using Exiled.API.Features;
 using Exiled.API.Interfaces;
+using JetBrains.Annotations;
+using MEC;
 using Mirror;
 using Mistaken.API.Diagnostics;
+using Mistaken.API.Extensions;
+using Mistaken.API.GUI;
 using UnityEngine;
 
 namespace Mistaken.API.Toys
@@ -27,13 +34,13 @@ namespace Mistaken.API.Toys
         /// <param name="syncPosition">Should toy's position be sync once every frame.</param>
         /// <param name="movementSmoothing">Toy's movementSmoothing.</param>
         /// <returns>Spawned toy.</returns>
-        public static PrimitiveObjectToy SpawnPrimitive(PrimitiveType type, Transform parent, Color color, bool hasCollision, bool syncPosition, byte? movementSmoothing = null)
+        public static PrimitiveObjectToy SpawnPrimitive(PrimitiveType type, Transform parent, Color color, bool hasCollision, bool syncPosition, byte? movementSmoothing, [CanBeNull] MeshRenderer meshRenderer)
         {
             var toy = SpawnBase(PrimitiveBaseObject, parent, movementSmoothing);
 
-            var primitiveObjectToy = InitializePrimitiveSource(toy, type, color, hasCollision);
+            var primitiveObjectToy = InitializePrimitive(toy, type, color, hasCollision, syncPosition, meshRenderer);
 
-            FinishSpawningToy(toy, syncPosition);
+            FinishSpawningToy(toy);
 
             return primitiveObjectToy;
         }
@@ -49,12 +56,12 @@ namespace Mistaken.API.Toys
         /// <param name="syncPosition">Should toy's position be sync once every frame.</param>
         /// <param name="movementSmoothing">Toy's movementSmoothing.</param>
         /// <returns>Spawned toy.</returns>
-        public static PrimitiveObjectToy SpawnPrimitive(PrimitiveType type, Vector3 position, Quaternion rotation, Vector3 scale, Color color, bool syncPosition, byte? movementSmoothing = null)
+        public static PrimitiveObjectToy SpawnPrimitive(PrimitiveType type, Vector3 position, Quaternion rotation, Vector3 scale, Color color, bool syncPosition, byte? movementSmoothing, [CanBeNull] MeshRenderer meshRenderer)
         {
             var toy = SpawnBase(PrimitiveBaseObject, position, rotation, scale, movementSmoothing);
-            var primitiveObjectToy = InitializePrimitiveSource(toy, type, color, null);
+            var primitiveObjectToy = InitializePrimitive(toy, type, color, null, syncPosition, meshRenderer);
 
-            FinishSpawningToy(toy, syncPosition);
+            FinishSpawningToy(toy);
 
             return primitiveObjectToy;
         }
@@ -74,9 +81,9 @@ namespace Mistaken.API.Toys
         {
             var toy = SpawnBase(PrimitiveBaseLight, parent, movementSmoothing);
 
-            var lightSourceToy = InitializeLightSource(toy, color, intensity, range, shadows);
+            var lightSourceToy = InitializeLightSource(toy, color, intensity, range, shadows, syncPosition);
 
-            FinishSpawningToy(toy, syncPosition);
+            FinishSpawningToy(toy);
 
             return lightSourceToy;
         }
@@ -98,9 +105,27 @@ namespace Mistaken.API.Toys
         {
             var toy = SpawnBase(PrimitiveBaseLight, position, rotation, scale, movementSmoothing);
 
-            var lightSourceToy = InitializeLightSource(toy, color, intensity, range, shadows);
+            var lightSourceToy = InitializeLightSource(toy, color, intensity, range, shadows, syncPosition);
 
-            FinishSpawningToy(toy, syncPosition);
+            FinishSpawningToy(toy);
+
+            return lightSourceToy;
+        }
+
+        /// <summary>
+        /// Spawns light source admin toy.
+        /// </summary>
+        /// <param name="original">Light to mimic.</param>
+        /// <param name="syncPosition">Should toy's position be sync once every frame.</param>
+        /// <param name="movementSmoothing">Toy's movementSmoothing.</param>
+        /// <returns>Spawned toy.</returns>
+        public static LightSourceToy SpawnLight(Light original, bool syncPosition, byte? movementSmoothing = null)
+        {
+            var toy = SpawnBase(PrimitiveBaseLight, original.transform, movementSmoothing);
+
+            var lightSourceToy = InitializeLightSource(toy, original, syncPosition);
+
+            FinishSpawningToy(toy);
 
             return lightSourceToy;
         }
@@ -117,19 +142,26 @@ namespace Mistaken.API.Toys
         /// <inheritdoc/>
         public override void OnEnable()
         {
-            Exiled.Events.Handlers.Server.RestartingRound += PostRoundCleanup;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += PostRoundCleanup;
+            Exiled.Events.Handlers.Player.Verified += Player_Verified;
         }
 
         /// <inheritdoc/>
         public override void OnDisable()
         {
-            Exiled.Events.Handlers.Server.RestartingRound -= PostRoundCleanup;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= PostRoundCleanup;
+            Exiled.Events.Handlers.Player.Verified -= Player_Verified;
         }
 
         internal static readonly HashSet<AdminToyBase> SyncToyPosition = new HashSet<AdminToyBase>();
 
+        private static readonly Dictionary<Room, SynchronizerControllerScript> Controllers =
+            new Dictionary<Room, SynchronizerControllerScript>();
+
         private static LightSourceToy primitiveBaseLight;
         private static PrimitiveObjectToy primitiveBaseObject;
+
+        private static GlobalSynchronizerControllerScript globalController;
 
         private static PrimitiveObjectToy PrimitiveBaseObject
         {
@@ -193,28 +225,55 @@ namespace Mistaken.API.Toys
             return toy;
         }
 
-        private static void FinishSpawningToy(AdminToyBase toy, bool syncPosition)
+        private static void FinishSpawningToy(AdminToyBase toy)
         {
             NetworkServer.Spawn(toy.gameObject);
 
-            if (syncPosition)
-                SyncToyPosition.Add(toy);
-            else
-                toy.UpdatePositionServer();
+            toy.UpdatePositionServer();
         }
 
-        private static LightSourceToy InitializeLightSource(AdminToyBase toy, Color color, float intensity, float range, bool shadows)
+        private static LightSourceToy InitializeLightSource(AdminToyBase toy, Color color, float intensity, float range, bool shadows, bool syncPosition)
         {
             var lightSourceToy = toy.GetComponent<LightSourceToy>();
+            lightSourceToy._light.color = color;
+            lightSourceToy._light.intensity = intensity;
+            lightSourceToy._light.range = range;
+            lightSourceToy._light.shadows = shadows ? LightShadows.Soft : LightShadows.None;
+
             lightSourceToy.NetworkLightColor = color;
             lightSourceToy.NetworkLightIntensity = intensity;
             lightSourceToy.NetworkLightRange = range;
             lightSourceToy.NetworkLightShadows = shadows;
 
+            var syncScript = lightSourceToy.gameObject.AddComponent<LightSynchronizerScript>();
+            syncScript.Toy = lightSourceToy;
+            syncScript.SyncPosition = syncScript.SyncRotation = syncScript.SyncScale = syncPosition;
+            (toy.GetComponentInParent<SynchronizerControllerScript>() ?? globalController).AddScript(syncScript);
+
             return lightSourceToy;
         }
 
-        private static PrimitiveObjectToy InitializePrimitiveSource(AdminToyBase toy, PrimitiveType type, Color color, bool? hasCollision)
+        private static LightSourceToy InitializeLightSource(AdminToyBase toy, Light original, bool syncPosition)
+        {
+            var lightSourceToy = toy.GetComponent<LightSourceToy>();
+
+            lightSourceToy.NetworkLightColor = original.color;
+            lightSourceToy.NetworkLightIntensity = original.intensity;
+            lightSourceToy.NetworkLightRange = original.range;
+            lightSourceToy.NetworkLightShadows = original.shadows != LightShadows.None;
+
+            var syncScript = lightSourceToy.gameObject.AddComponent<LightSynchronizerScript>();
+            syncScript.Toy = lightSourceToy;
+
+            (toy.GetComponentInParent<SynchronizerControllerScript>() ?? globalController).AddScript(syncScript);
+
+            syncScript.ClonedLight = original;
+            syncScript.SyncPosition = syncScript.SyncRotation = syncScript.SyncScale = syncPosition;
+
+            return lightSourceToy;
+        }
+
+        private static PrimitiveObjectToy InitializePrimitive(AdminToyBase toy, PrimitiveType type, Color color, bool? hasCollision, bool syncPosition, [CanBeNull] MeshRenderer syncColor)
         {
             var primitiveObjectToy = toy.GetComponent<PrimitiveObjectToy>();
             primitiveObjectToy.NetworkPrimitiveType = type;
@@ -227,14 +286,120 @@ namespace Mistaken.API.Toys
                     Math.Abs(toy.transform.lossyScale.y),
                     Math.Abs(toy.transform.lossyScale.z));
                 toy.NetworkScale *= hasCollision.Value ? 1 : -1;
+
+                if (!hasCollision.Value && toy.transform.localScale.x > 0 && (type == PrimitiveType.Plane || type == PrimitiveType.Quad))
+                {
+                    toy.transform.eulerAngles += Vector3.right * 180;
+                    Exiled.API.Features.Log.Info("Rotated 180° X to compensate for negative scale");
+                }
+
+#warning Potential Bugs ?
+                toy.transform.localScale = new Vector3(
+                    Math.Abs(toy.transform.localScale.x),
+                    Math.Abs(toy.transform.localScale.y),
+                    Math.Abs(toy.transform.localScale.z));
+                toy.transform.localScale *= hasCollision.Value ? 1 : -1;
             }
+
+            var syncScript = primitiveObjectToy.gameObject.AddComponent<PrimitiveSynchronizerScript>();
+            syncScript.Toy = primitiveObjectToy;
+            syncScript.SyncPosition = syncScript.SyncRotation = syncScript.SyncScale = syncPosition;
+            syncScript.MeshRenderer = syncColor;
+
+            (toy.GetComponentInParent<SynchronizerControllerScript>() ?? globalController).AddScript(syncScript);
 
             return primitiveObjectToy;
         }
 
         private static void PostRoundCleanup()
         {
+            globalController = Server.Host.GameObject.AddComponent<GlobalSynchronizerControllerScript>();
             SyncToyPosition.Clear();
+
+            foreach (var room in Room.List)
+                Controllers[room] = room.gameObject.AddComponent<SynchronizerControllerScript>();
+
+            Module.RunSafeCoroutine(SynchronizationHandler(), nameof(SynchronizationHandler));
+            Module.RunSafeCoroutine(DEBUG(), nameof(DEBUG));
+        }
+
+        private static IEnumerator<float> SynchronizationHandler()
+        {
+            Dictionary<Player, Room> lastRooms = new Dictionary<Player, Room>();
+
+            while (!Round.IsStarted)
+                yield return Timing.WaitForSeconds(1f);
+
+            while (Round.IsStarted)
+            {
+                yield return Timing.WaitForSeconds(1f);
+
+                foreach (var player in RealPlayers.List)
+                {
+                    var curRoom = player.CurrentRoom;
+
+                    if (lastRooms.TryGetValue(player, out var lastRoom) && lastRoom == curRoom)
+                        continue; // Skip, room didn't change since last update
+                    Exiled.API.Features.Log.Debug($"Room changed, {lastRoom?.Type.ToString() ?? "NONE"} -> {curRoom?.Type.ToString() ?? "NONE"}");
+                    lastRooms[player] = curRoom;
+
+                    var room = Utilities.Room.Get(curRoom);
+
+                    if (room == null)
+                    {
+                        foreach (var item in Controllers.Values)
+                            item.RemoveSubscriber(player);
+
+                        continue;
+                    }
+
+                    var otherRooms = room.FarNeighbors;
+
+                    HashSet<SynchronizerControllerScript> toSync = NorthwoodLib.Pools.HashSetPool<SynchronizerControllerScript>.Shared.Rent();
+
+                    if (Controllers.TryGetValue(room.ExiledRoom, out var script))
+                        toSync.Add(script);
+
+                    // ReSharper disable once LoopCanBeConvertedToQuery
+                    foreach (var item in otherRooms)
+                    {
+                        if (Controllers.TryGetValue(item.ExiledRoom, out script))
+                            toSync.Add(script);
+                    }
+
+                    foreach (var item in Controllers.Values.Where(x => !toSync.Contains(x)))
+                        item.RemoveSubscriber(player);
+
+                    foreach (var item in toSync)
+                        item.AddSubscriber(player);
+
+                    NorthwoodLib.Pools.HashSetPool<SynchronizerControllerScript>.Shared.Return(toSync);
+                }
+            }
+        }
+
+        private static IEnumerator<float> DEBUG()
+        {
+            yield return Timing.WaitForSeconds(1f);
+
+            while (Round.IsStarted)
+            {
+                yield return Timing.WaitForSeconds(1f);
+
+                foreach (var player in RealPlayers.List)
+                {
+                    player.SetGUI("structures_debug",
+                        PseudoGUIPosition.TOP,
+                        string.Join("\n",
+                            Controllers.Where(x => x.Value.IsSubscriber(player))
+                                .Select(x => x.Key.Type + $" ({x.Key.Position})")));
+                }
+            }
+        }
+
+        private static void Player_Verified(Exiled.Events.EventArgs.VerifiedEventArgs ev)
+        {
+            globalController.SyncFor(ev.Player);
         }
     }
 }
